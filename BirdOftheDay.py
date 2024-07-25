bb55d7513aec190e5ddf132059788be2b8d2e88e
quinnb48 <qbehrens@reed.edu>
2024-07-25 11:11:57 -0700

    milestone #1

--- BirdOftheDay.py
+++ BirdOftheDay.py
@@ -7,17 +7,26 @@ time.sleep(1)
 #p = open("BotD.txt", "w")
 #p.write("run")
 #p.close()
-print("Welcome to Bird of the Day! The current Bird of the Day is...")
+print("Welcome to Bird of the Day!")
 while True:
-    time.sleep(3)
-    p = open("BotD.txt", "r")
-    text = p.readlines()
-    if text[0] != "run" and text[0].isnumeric() == False:
-        print(f"Bird name: {text[0]}")
-        print(f"Bird image: {text[1]}")
-        print(f"Conservation status: {text[2]}")
-        print(f"Wiki link: {text[3]}")
-        #p.close()
-        #break
+    inp = input("Enter B to view the Bird of the Day\nEnter H to view Bird of the Day history\nEnter A to view about page\nEnter Q to quit\n")
 
-    p.close()
+#while True:
+    if inp == "B" or inp == "b":
+        p = open("BotD.txt", "r")
+        text = p.readlines()
+        if text[0] != "run" and text[0].isnumeric() == False:
+            print(f"Bird name: {text[0]}")
+            print(f"Bird image: {text[1]}")
+            print(f"Conservation status: {text[2]}")
+            print(f"Wiki link: {text[3]}")
+            #p.close()
+            #break
+        p.close()
+        time.sleep(3)
+    elif inp == "H" or inp == "h":
+        print("History page: TODO")
+    elif inp == "A" or inp == "a":
+        print("About page: TODO")
+    elif inp == "Q" or inp == "q":
+        break
